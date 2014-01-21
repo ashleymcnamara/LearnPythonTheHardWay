a50a71371621d3de0cd44e1510aa4e304136dd9a
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-20 19:34:05 -0800

    Exercise 29: What If

---
+++ ex29.py
@@ -0,0 +1,32 @@
+people = 20
+joes = 30
+dogs = 15
+true = 1
+false = 0
+
+if true < false:
+	print "Does true = false?"
+
+if people < joes:
+	print "Too many joes! The world is doomed!"
+
+if people > joes:
+	print "Not many joes! The world is saved!"
+
+if people < dogs:
+	print "The world is drooled on!"
+
+if people > dogs:
+	print "The world is dry!"
+
+
+dogs += 2
+
+if people >= dogs:
+	print "People are greater than or equal to dogs."
+
+if people <= dogs:
+	print "People are less than or equal to dogs."
+
+if people == dogs:
+	print "People are dogs"
