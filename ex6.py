80f6b6ea843e22a2114512d0c30c5ccea00687a3
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-20 19:49:05 -0800

    Exercise 6: Strings and Text

--- ex6.py
+++ ex6.py
@@ -1,5 +1,20 @@
-my_weight = 120
-kilos = 0.453592
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who know %s." % (binary, do_not)
 
-print "I am %d lbs" % my_weight
-print "That is %d in kilos" % (my_weight * kilos)
+print x
+print y
+
+print "I said: %r." % x
+print "I also said: %s . " % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious 
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
