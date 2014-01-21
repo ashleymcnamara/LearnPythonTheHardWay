b54fa0578cafeed7f4e29aa85488c6fdfb4d4cf3
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-21 08:20:01 -0800

    EXERCISE 3 - STUDY DRILLS

---
+++ ex3_studydrills.py
@@ -0,0 +1,33 @@
+# Study Drills
+
+# Above each line, use the # to write a comment to yourself explaining what the line does.
+# Remember in Exercise 0 when you started Python? Start Python this way again and using the above characters and what you know, use Python as a calculator.
+# Find something you need to calculate and write a new .py file that does it.
+# Notice the math seems "wrong"? There are no fractions, only whole numbers. Find out why by researching what a "floating point" number is.
+# Rewrite ex3.py to use floating point numbers so it's more accurate (hint: 20.0 is floating point).
+
+print "I will now count my chickens:"
+# Giving values to "Hens" and "Roosters"
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+# Giving python the calculations for "eggs"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# This exercise is basically showing you the math capabilities within python.
