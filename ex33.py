9e13e85ae024ed3d9a22cc7c331117550c274141
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-20 19:37:50 -0800

    Exercise 33: While Loops

---
+++ ex33.py
@@ -0,0 +1,18 @@
+i = 0
+numbers = []
+red = 2
+
+
+while i < red:
+	print "at the top i is %d" % i 
+	numbers.append(i)
+
+	i = i + 7
+	print "numbers now:", numbers
+	print "at the bottom i is %d" % i
+
+
+print "the numbers: "
+
+for range in numbers:
+	print [2, 4, 3]
