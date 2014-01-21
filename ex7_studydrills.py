ddb7aa2f095b02fcc56a7b5e057259d616c60fc4
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-21 10:23:37 -0800

    EXERCISE 7 - STUDY DRILLS

---
+++ ex7_studydrills.py
@@ -0,0 +1,38 @@
+# Study Drills
+
+# For these next few exercises, you will have the exact same Study Drills.
+
+# Go back through and write a comment on what each line does.
+# Read each one backward or out loud to find your errors.
+# From now on, when you make mistakes write down on a piece of paper what 
+# kind of mistake you made.
+# When you go to the next exercise, look at the last mistakes you made
+# and try not to make them in this new one.
+# Remember that everyone makes mistakes. Programmers are like magicians
+# who like everyone to think they are perfect and never wrong, but 
+# it's all an act. They make mistakes all the time.
+
+# Lines 16 - 20 print "Mary had a little lamb" but in line 17 the "%s" is used to insert "snow"
+# and in line 20 we are saying print the '.' ten times.
+print "Mary had a little lamb." 
+print "it's fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 
+# Lines 22 - 33 are defining variables. end1 - end 12 when combined spell 
+# "cheeseburger"
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Lines 37 - 38 print end1 - end12 by adding them all together.
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
