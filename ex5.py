becc339026ccb0e8191a55dcbb94c7c9ab54d436
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-20 18:53:29 -0800

    Exercise 5: More Variables And Printing

---
+++ ex5.py
@@ -0,0 +1,16 @@
+name = 'Ashley N. McNamara'
+age = 32 
+height = 55 
+weight = 120 
+eyes = 'blue'
+teeth = 'white'
+hair = 'blonde'
+
+print "Let's talk about %s." % name
+print "She's %d inches tall." % height
+print "She's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." % (eyes, hair)
+print "Her teeth are usually %s depending on the soda." % teeth
+
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
