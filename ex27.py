e5939e208b2d16dc622e1e62d7b3f0f4f1e1f336
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-20 19:31:32 -0800

    Exercise 27: Memorizing Logic

---
+++ ex27.py
@@ -0,0 +1,38 @@
+True and True
+# MY GUESS IS TRUE
+1 == 1 and 2 == 2
+# MY GUESS IS TRUE
+"test" == "test"
+# MY GUESS IS TRUE
+1 == 1 or 2 != 1
+# MY GUESS IS TRUE
+True and 1 == 1
+# MY GUESS IS TRUE
+3 != 1
+#MY GUESS IS TRUE
+True or 1 == 1
+# MY GUESS IS TRUE
+"testing" == "testing"
+# MY GUESS IS TRUE
+1 != 0 and 2 == 2
+# MY GUESS IS TRUE
+"test" != "testing"
+# MY GUESS IS TRUE
+"test" == "test"
+# MY GUESS IS TRUE
+not (True and False)
+# MY TRUE
+not (1 == 2 and 0 != 1)
+# MY GUESS IS TRUE
+not (0 == 1 or 1000 == 1090)
+# MY GUESS IS TRUE
+not (10 != 10 or 3 == 5)
+# MY GUESS IS TRUE
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+# MY GUESS IS TRUE
+1 == 1 and not ("testing" == 1 or 1 == 0)
+# MY GUESS IS TRUE
+"bacon" == "bacon" and not (3 == 4 or 3 == 2)
+# MY GUESS IS TRUE
+3 == 3 and not ("testing" == "Python" or "testing" == "Python")
+# MY GUESS IS TRUE
