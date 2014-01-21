62855a4a6ed8ba0b34161f2bdf014355a52d0788
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-20 19:50:04 -0800

    Exercise 7: More Printing

--- ex7.py
+++ ex7.py
@@ -1,20 +1,21 @@
-x = "There are %d types of people." % 10
-binary = "binary"
-do_not = "don't"
-y = "Those who know %s and those who know %s." % (binary, do_not)
+print "Mary had a little lamb." 
+print "it's fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # What'd that do?
 
-print x
-print y
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
 
-print "I said: %r." % x
-print "I also said: %s . " % y
-
-hilarious = False
-joke_evaluation = "Isn't that joke so funny?! %r"
-
-print joke_evaluation % hilarious 
-
-w = "This is the left side of..."
-e = "a string with a right side."
-
-print w + e
+# Watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
