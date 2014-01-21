4ef70650600358a672a9124f5a6bb8407c0a5c20
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-01-21 08:14:10 -0800

    EXERCISE 1 - STUDY DRILLS

---
+++ ex1_studydrills.py
@@ -0,0 +1,20 @@
+#Study Drills
+
+#You will also have Study Drills. The Study Drills contain things you should try to do. If you can't, skip it and come back later.
+
+#For this exercise, try these things:
+
+#Make your script print another line.
+#Make your script print only one of the lines.
+#Put a '#' (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+
+print "Hello World!" #(The # Character omits the line from the code and treats it as a "note")
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print ' I "said" do not touch this.'
+print "Making my script print another line"
+
+# to print only one line us the "#" to omit the line.
